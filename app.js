a7675ff6ea9a813b3f24311a4292c6223ea874c8
Akshay Chalana <ac2zoom@yahoo.com>
2015-05-23 19:32:24 -0700

    app initialized.  Swig integration in the works

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express"),
+    app = express(),
+    cons = require("consolidate"),
+    facebook = require('./facebook.js'),
+    https = require('https');
+
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+ 
+var query = "";
+
+var friends = "";
+
+var accessToken = "CAACEdEose0cBANa2AOJKUwj4KhZAt6xPWnYQa6zqbag3dNdcjRaHfSlcTtVVfH4nGD5ZCdRD55ic6ZBmMGHFK3tR3UZBEIi5By5VBBblTjL0XqYiHMUYSKjfEUh92spLeylcnX6pGWgEvyNeXc2WsdGsURpvcHUfoRMVfT8Iz1yOaaNVmC2th1ZA3WekC6DRhBrYWkI6TkQvbhdfaEfFU";  //TODO
+
+facebook.getFbData(accessToken, '/me/friends?fields=name,id,picture', function(data){
+    friends = data;
+});
